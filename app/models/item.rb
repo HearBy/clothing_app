583c839aec79a6c6475979e6f5a65996d0b98c5c
David Shuck <deshuck@gmail.com>
2013-10-01 22:19:51 -0400

    add header

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,8 @@
 class Item < ActiveRecord::Base
 	def self.sizer(waist_size, front_rise, thigh, knee, leg_opening)
 		if waist_size
-			where('waist <= ?', (waist_size.to_d + 1))
-			.where('waist >= ?', (waist_size.to_d - 1))
+			where('waist <= ?', (waist_size.to_d + 0.25))
+			.where('waist >= ?', (waist_size.to_d - 0.5))
 			.where("ABS(front_rise/waist - ?) < 0.03", front_rise)
 			.where("ABS(thigh/waist - ?) < 0.03", thigh)
 			.where("ABS(knee/waist - ?) < 0.03", knee)
