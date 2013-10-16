9417221d3abb1d365e164418bc30e6f22d85815d
Matthew Star <your.mattjstar@gmail.com>
2013-10-16 13:07:36 -0400

    merge with david

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-ruby '2.0.0' #ruby-gemset=railstutorial_rails_4_0
+
 gem 'rails', '4.0.0'
 gem 'anjlab-bootstrap-rails', :require => 'bootstrap-rails',
                           :github => 'anjlab/bootstrap-rails',
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,15 @@ class Item < ActiveRecord::Base
 		if waist_size
 			where('waist <= ?', (waist_size.to_d + 0.25))
 			.where('waist >= ?', (waist_size.to_d - 0.5))
-			.where("ABS(front_rise/waist - ?) < 0.03", front_rise)
 			.where("ABS(thigh/waist - ?) < 0.03", thigh)
 			.where("ABS(knee/waist - ?) < 0.03", knee)
 			.where("ABS(leg_opening/waist - ?) < 0.03", leg_opening)
+			.where("ABS(front_rise/waist - ?) < 0.03", front_rise)
+
+			# .where("ABS(front_rise/waist - ?) < 0.03", front_rise)
+			# .where("ABS(thigh/waist - ?) < 0.03", thigh)
+			# .where("ABS(knee/waist - ?) < 0.03", knee)
+			# .where("ABS(leg_opening/waist - ?) < 0.03", leg_opening)
 		else
 			all
 		end
